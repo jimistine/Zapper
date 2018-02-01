52f300f236316aecb83d4fc8245c1f6c37e69cc7
Unknown <emm615@nyu.edu>
2018-02-01 14:36:39 -0500

    Erik's Added Scene Movments and Charging

---
+++ ZapperProject/Assets/Scripts/Erik/Wires.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wires : MonoBehaviour {
+    public float PlayersStartPositionX;
+    public float PlayersStartPositionY;
+    public bool PlayerStartRight = true;
+    public float AnchorLeft = -6f;
+    public float AnchorRight = 6f;
+    public float StartPositionLeft;
+    public float StartPositionRight;
+    public float PlayerPositionOffsetY;
+
+    // Use this for initialization
+    void Start () {
+
+        StartPositionLeft = AnchorLeft;
+        StartPositionRight = AnchorRight;
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (PlayerStartRight == true)
+        {
+            PlayersStartPositionX = StartPositionRight;
+        }
+        if (PlayerStartRight == false)
+        {
+            PlayersStartPositionX = StartPositionLeft;
+        }
+
+        PlayersStartPositionY = transform.position.y + PlayerPositionOffsetY;
+
+    }
+}
