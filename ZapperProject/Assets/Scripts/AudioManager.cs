341df8a4e2bc14e686500cdd370ba47dbee52a6a
Jimi Stine <jrstine@me.com>
2018-02-12 11:39:18 -0500

    Set up audio manager,
    and implemented a few basic sound effects.

---
+++ ZapperProject/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+
+	public AudioSource HitCrow_source;
+	public AudioSource ShootCharge_source;
+	public AudioSource Explosion_source;
+
+	public AudioClip ShootCharge;
+	public AudioClip Explosion;
+	public AudioClip HitCrow;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
