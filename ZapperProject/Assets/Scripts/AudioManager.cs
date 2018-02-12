f6988dff32e17a8516fb1eb2e0123d55fa694f8e
Jimi Stine <jrstine@me.com>
2018-02-12 12:08:56 -0500

    Added VO,
    as well as some stuff to manage it with in the audio manager.

--- ZapperProject/Assets/Scripts/AudioManager.cs
+++ ZapperProject/Assets/Scripts/AudioManager.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
-
+	public int VO_Delay;
+	
 	public AudioSource HitCrow_source;
 	public AudioSource ShootCharge_source;
 	public AudioSource Explosion_source;
+	public AudioSource VO_source;
 
 	public AudioClip ShootCharge;
 	public AudioClip Explosion;
 	public AudioClip HitCrow;
+	public AudioClip VO;
 	
 	// Use this for initialization
 	void Start () {
-		
+		VO_source.PlayDelayed(VO_Delay);
 	}
 	
 	// Update is called once per frame
