9abf31130d3d5edf5e9787153efd9a2a2164daaf
Jimi Stine <jrstine@me.com>
2018-03-07 12:40:02 -0500

    Eldits b4 level design
    building out scene advancement

---
+++ ZapperProject/Assets/SceneAdvancer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+public class SceneAdvancer : MonoBehaviour
+{
+
+	public GameObject MemoryOBJ;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		MemoryOBJ = GameObject.FindGameObjectWithTag("Memory");
+	}
+
+	public void CheckStateToLoad()
+	{
+		if (MemoryOBJ.GetComponent<Memory>().PlayedFirstRound &&
+		    MemoryOBJ.GetComponent<Memory>().PlayedSecondRound == false)
+		{
+			Flowchart.BroadcastFungusMessage ("Load Round 2");
+		}
+	}
+}
