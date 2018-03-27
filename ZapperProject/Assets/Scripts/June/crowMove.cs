f4da9adab74f22db5f45e32316b6e5dfe78692ec
Unknown <emm615@nyu.edu>
2018-03-27 14:08:01 -0500

    Fixed enemy rock bug
    enemies now only fail if they are on the right wire.

--- ZapperProject/Assets/Scripts/June/crowMove.cs
+++ ZapperProject/Assets/Scripts/June/crowMove.cs
@@ -63,7 +63,7 @@ public class crowMove : MonoBehaviour {
         if (isRock == true)
         {
             transform.Translate(0, -goSpeed, 0);
-            if (transform.position.y < SC.PlayerObject.transform.position.y + 0.5f && transform.position.y > SC.PlayerObject.transform.position.y - 0.5f)
+            if (transform.position.y < SC.PlayerObject.transform.position.y + 0.5f && transform.position.y > SC.PlayerObject.transform.position.y - 0.5f && CurrentWire == SC.PlayerObject.GetComponent<ChracterController>().PlayerCurrentWire)
             {
                 FailStateCrow();
                 //Destroy bird for now will need a fail state animation for birds hittitng fuze box
