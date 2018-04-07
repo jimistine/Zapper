1bbe664b6a00d42a4baa8b201a5d9172ef5013f4
Jimi Stine <jrstine@me.com>
2018-04-07 19:20:18 -0400

    Second pass of intro
    A whole buncha tweakin'

--- ZapperProject/Assets/Scripts/Jimi/AudioManager.cs
+++ ZapperProject/Assets/Scripts/Jimi/AudioManager.cs
@@ -16,19 +16,10 @@ public class AudioManager : MonoBehaviour
 	public AudioClip Shoot;
 	public AudioClip Fail;
 	public AudioClip Hit;
-	public AudioClip VOClip_1;
-	public AudioClip VOClip_2;
-	public AudioClip VOClip_3;
-	public AudioClip VOClip_4;
-	public AudioClip VOClip_5;
-	public AudioClip VOClip_6;
-	public AudioClip VOClip_7;
-	public AudioClip VOClip_8;
-	public AudioClip VOClip_9;
 	
-	public void VO_1()
-	{ VO_source.PlayOneShot(VOClip_1);}
-	
-	public void VO_2()
-	{ VO_source.PlayOneShot(VOClip_2);}
+//	public void VO_1()
+//	{ VO_source.PlayOneShot(VOClip_1);}
+//	
+//	public void VO_2()
+//	{ VO_source.PlayOneShot(VOClip_2);}
 }
