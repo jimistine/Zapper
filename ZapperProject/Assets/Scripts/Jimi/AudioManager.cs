9e4929fd540d5b2ed79d410da94a9cee7f3860f7
Jimi Stine <jrstine@me.com>
2018-04-10 00:56:03 -0400

    Added SFX to crunch polish throughout

--- ZapperProject/Assets/Scripts/Jimi/AudioManager.cs
+++ ZapperProject/Assets/Scripts/Jimi/AudioManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -12,14 +13,44 @@ public class AudioManager : MonoBehaviour
 	public AudioSource Shoot_source;
 	public AudioSource FailSound_source;
 	public AudioSource VO_source;
+	public AudioSource Music_source;
+	public AudioSource Ambient_source;
 
+	
 	public AudioClip Shoot;
 	public AudioClip Fail;
 	public AudioClip Hit;
+	public AudioClip Music;
+	public AudioClip Ambient;
+
+	public AudioMixerSnapshot Wagner;
+	public AudioMixerSnapshot NormalAmbient;
 	
 //	public void VO_1()
 //	{ VO_source.PlayOneShot(VOClip_1);}
 //	
 //	public void VO_2()
 //	{ VO_source.PlayOneShot(VOClip_2);}
+
+	public void Play_Music()
+	{
+		Music_source.Play();
+		Wagner.TransitionTo(1);
+	}
+
+	public void Stop_Muisc()
+	{
+		Music_source.Stop();
+		NormalAmbient.TransitionTo(1);
+	}
+	
+	public void Play_Ambient()
+	{
+		Ambient_source.Play();
+	}
+
+	public void Stop_Ambient()
+	{
+		Ambient_source.Stop();
+	}
 }
