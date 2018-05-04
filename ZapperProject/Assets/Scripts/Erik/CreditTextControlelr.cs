b8976204e29c6aff6b32438089be5c78683a70e1
Unknown <emm615@nyu.edu>
2018-05-04 02:43:23 -0500

    Added credit functionality
    new script can run a full text one character at a time. will need to tune interval lengths.

---
+++ ZapperProject/Assets/Scripts/Erik/CreditTextControlelr.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditTextControlelr : MonoBehaviour {
+
+    //Jimi Stine's Zapper
+    //By Jimi Stine 
+    //Hello World
+
+    public string FullTextBox;
+    public float IntervalPerCharMin;
+    public float IntervalPerCharMax;
+    public float StoreStartTime;
+    public bool IsStarted = false;
+    string[] ConvertedText; 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(DelayPrintNext());
+        }
+		
+	}
+
+    public void StartTextTime()
+    {
+        StoreStartTime = Time.time;
+    }
+    public void ConvertStringToCharacters()
+
+    {
+        //ConvertedText = new string[FullTextBox.Length];
+        //for (int i = 0; i < FullTextBox.Length; i++)
+        //{
+        //    ConvertedText[i] = System.Convert.ToString(FullTextBox[i]);
+        //}
+
+        //foreach(string x in ConvertedText)
+        //{
+        //    if (x == "]")
+        //    {
+        //        StartCoroutine(DelayPrintNext(x));
+        //        //Debug.Log("\n");
+        //        //gameObject.GetComponent<Text>().text += "\n";
+        //    }
+        //    else
+        //    {
+        //        StartCoroutine(DelayPrintNext(x));
+        //        //Debug.Log(x);
+        //        //gameObject.GetComponent<Text>().text += x;
+        //    }
+        //}
+    }
+    IEnumerator DelayPrintNext()
+    {
+        Debug.Log("Start");
+        
+        ConvertedText = new string[FullTextBox.Length];
+
+        for (int i = 0; i < FullTextBox.Length; i++)
+        {
+            ConvertedText[i] = System.Convert.ToString(FullTextBox[i]);
+        }
+
+        foreach (string x in ConvertedText)
+        {
+            if (x == "]")
+            {
+                yield return new WaitForSeconds(Random.Range(IntervalPerCharMin, IntervalPerCharMax));
+                //StartCoroutine(DelayPrintNext(x));
+                //Debug.Log("\n");
+                gameObject.GetComponent<Text>().text += "\n";
+            }
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(IntervalPerCharMin, IntervalPerCharMax));
+                //StartCoroutine(DelayPrintNext(x));
+                //Debug.Log(x);
+                gameObject.GetComponent<Text>().text += x;
+            }
+        }
+        Debug.Log("Finish");
+    }
+}
